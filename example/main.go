2e51aeb12e02d08b9c1d19351a1abe5ea5949215
SmartWalle <smartwalle@gmail.com>
2025-04-11 15:59:30 +0800

    Context 调整

--- example/main.go
+++ example/main.go
@@ -7,8 +7,11 @@ import (
 )
 
 func main() {
-	var app = bootstrap.New(bootstrap.WithServers(&Server{id: "1"}, &Server{id: "2"}, &Server{id: "3"}))
-
+	var ctx = NewContext(context.Background(), "hello")
+	var app = bootstrap.New(
+		bootstrap.WithContext(ctx),
+		bootstrap.WithServers(&Server{id: "1"}, &Server{id: "2"}, &Server{id: "3"}),
+	)
 	fmt.Println(app.Run())
 }
 
@@ -17,11 +20,21 @@ type Server struct {
 }
 
 func (s *Server) Start(ctx context.Context) error {
-	fmt.Printf("%s  start \n", s.id)
+	fmt.Printf("%s  start: %+v \n", s.id, FromContext(ctx))
 	return nil
 }
 
 func (s *Server) Stop(ctx context.Context) error {
-	fmt.Printf("%s  stop \n", s.id)
+	fmt.Printf("%s  stop: %+v \n", s.id, FromContext(ctx))
 	return nil
+}
+
+type contextKey struct{}
+
+func NewContext(ctx context.Context, value interface{}) context.Context {
+	return context.WithValue(ctx, contextKey{}, value)
+}
+
+func FromContext(ctx context.Context) interface{} {
+	return ctx.Value(contextKey{})
 }
